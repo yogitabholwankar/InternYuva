bd2f0411efeb3ff6607671a2cf49774bce178b0c
Desh Deepak <deshdeepakxpm2@gmail.com>
2021-01-27 19:50:41 +0530

    changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+# from django.contrib.auth.models import User
+# from django.core.validators import RegexValidator
+# from django.utils import timezone
+# from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+
+# class CustomeUser(AbstractBaseUser, PermissionsMixin):
+# 	email = models.EmailField(_('email address'), unique=True)
+# 	is_staff = models.BooleanField(default=False)
+# 	is_active = models.BooleanField(default=False)
+# 	date_Joined = models.DateTimeField(default=timezone.now)
+
+# 	USERNAME_FIELD = 'email'
+# 	REQUIRED_FIELD = []
+
+# 	objects = CustomeUserManager()
+
+
+# 	def __str__(self):
+# 		return self.email
