46e1bdeab6eb34fe2ed66aefc895be3b80dda008
samir patil <samirspatil742099@gmail.com>
2021-03-27 18:06:46 +0530

    login form url

--- blog/migrations/0002_auto_20210305_1705.py
+++ blog/migrations/0002_auto_20210305_1705.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('blog', '0001_initial'),
     ]
+    atomic = False
 
     operations = [
         migrations.RenameModel(
